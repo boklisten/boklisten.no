6d52eb197d9d7705baa051fc49e586e5f0395468
Adrian Andersen <adrianandersen@protonmail.com>
2026-08-16 23:17:11 +0200

    fix: update dump and restore

--- cron_jobs/copy_prod_mongodb_to_staging/dump_and_restore.sh
+++ cron_jobs/copy_prod_mongodb_to_staging/dump_and_restore.sh
@@ -1,3 +1,2 @@
-mongodump --uri="${FROM_MONGODB_URI}" --db=production
-rm -rf dump/admin dump/prelude.json
+mongodump --uri="${FROM_MONGODB_URI}" --db=production --authenticationDatabase=admin
 mongorestore --drop --uri="${TO_MONGODB_URI}" --nsFrom="production.*" --nsTo="staging.*" dump/
